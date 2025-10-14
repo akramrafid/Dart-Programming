1322188b8a358717bc4a7342ba4268748afcab71
Akram Rafid <231017512@eastdelta.edu.bd>
2025-10-14 23:32:24 +0600

    main

---
+++ 02_variable.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  String name = 'Akram Rafid';
+  int age = 20;
+  double height = 1.75;
+  bool isStudent = true;
+  var city = 'Chattogram';
+  const country = 'Bangladesh';
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+  print('City: $city');
+  print('Country: $country');
+}
