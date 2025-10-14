d33d3f7b518f79210cdf3fd261ab6f2c033b0264
Akram Rafid <231017512@eastdelta.edu.bd>
2025-10-14 23:50:16 +0600

    main

---
+++ 02_variable_adv.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  var name = 'Akram Rafid'; // Inferred as String
+  final int age = 20; // Immutable, but can be set at runtime
+  const double height = 1.75; // Immutable and must be set at compile-time
+  bool isStudent = true; // Mutable variable
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+  
+
+}
